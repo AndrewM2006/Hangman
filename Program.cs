2eb43c9c12305ad3aef0e8c06aa8d715f95975ea
Andrew Monteith <339531824@GoTVDSB.ca>
2023-09-28 10:55:34 -0400

    need to start making class or something with wod list for different difficulties, I got custom working

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Hangman
@@ -10,6 +11,50 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
+            string gamemode="", word;
+            bool valid = false;
+            Console.WriteLine("1: Easy Mode");
+            Console.WriteLine("2: Medium Mode");
+            Console.WriteLine("3: Hard Mode");
+            Console.WriteLine("4: Custom");
+            while (gamemode == "")
+            {
+                gamemode=Console.ReadLine();
+                if (gamemode == "1")
+                {
+
+                }
+                else if (gamemode == "2")
+                {
+
+                }
+                else if (gamemode == "3")
+                {
+
+                }
+                else if (gamemode == "4")
+                {
+                    Console.Write("Enter Secret Word: ");
+                    while (!valid)
+                    {
+                        word = Console.ReadLine().ToUpper();
+                        if (Regex.IsMatch(word, @"^[a-zA-Z]+$"))
+                        {
+                            valid = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Word");
+                        }
+                    }
+                }
+                else
+                {
+                    gamemode = "";
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
