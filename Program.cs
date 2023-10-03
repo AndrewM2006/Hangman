65e82e31e1ce907447076ff10eda2ecd4ff588cf
Andrew Monteith <339531824@GoTVDSB.ca>
2023-10-03 10:43:17 -0400

    finishing touches completed

--- Program.cs
+++ Program.cs
@@ -14,11 +14,17 @@ namespace Hangman
         {
             string gamemode, word="", playAgain, progress="", guessString;
             bool valid, gameover;
-            int incorrect, indexReplace;
+            int incorrect, indexReplace, indexString;
             char guess='a';
+            ConsoleKeyInfo key;
             List <char> letters = new List <char> ();
+            Console.Title="Hangman";
+            Console.WindowWidth = 32;
+            Console.WindowHeight = 12;
+            Console.ForegroundColor = ConsoleColor.White;
             while (true)
             {
+                Console.BackgroundColor = ConsoleColor.Black;
                 Console.Clear();
                 Word newword = new Word();
                 gamemode = "";
@@ -49,10 +55,26 @@ namespace Hangman
                     }
                     else if (gamemode == "4")
                     {
-                        Console.Write("Enter Secret Word: ");
+                        word = "";
+                        Console.WriteLine("Enter Secret Word: ");
                         while (!valid)
                         {
-                            word = Console.ReadLine().ToUpper();
+                            do
+                            {
+                                key = Console.ReadKey(true);
+                                if (key.Key == ConsoleKey.Backspace)
+                                {
+                                    Console.Write("\b \b");
+                                    word=word.Substring(0, word.Length - 1);
+                                }
+                                else if (key.Key != ConsoleKey.Enter)
+                                {
+                                    Console.Write("*");
+                                    word += key.KeyChar;
+                                }
+                            } while (key.Key != ConsoleKey.Enter);
+                            Console.WriteLine();
+                            word=word.ToUpper();
                             if (Regex.IsMatch(word, @"^[a-zA-Z]+$"))
                             {
                                 valid = true;
@@ -108,12 +130,14 @@ namespace Hangman
                         if (word.Contains(guess))
                         {
                             indexReplace = 0;
+                            indexString = -1;
                             foreach (char c in word)
                             {
                                 if (c == guess)
                                 {
-                                    indexReplace=(word.IndexOf(c, indexReplace))*3+1;
-                                    progress=progress.Remove(indexReplace, 1).Insert(indexReplace, Convert.ToString(guess));
+                                    indexString = word.IndexOf(c, indexString + 1);
+                                    indexReplace = indexString * 3 + 1;
+                                    progress = progress.Remove(indexReplace, 1).Insert(indexReplace, Convert.ToString(guess));
                                 }
                             }
                         }
@@ -123,8 +147,10 @@ namespace Hangman
                         }
                         if (!progress.Contains("_"))
                         {
+                            Console.BackgroundColor = ConsoleColor.Green;
                             Console.Clear();
                             Console.WriteLine("YOU WIN!!!");
+                            Music();
                             valid = false;
                             while (!valid)
                             {
@@ -175,11 +201,13 @@ namespace Hangman
                         if (word.Contains(guess))
                         {
                             indexReplace = 0;
+                            indexString = -1;
                             foreach (char c in word)
                             {
                                 if (c == guess)
                                 {
-                                    indexReplace = (word.IndexOf(c, indexReplace)) * 3 + 1;
+                                    indexString = word.IndexOf(c, indexString+1);
+                                    indexReplace = indexString * 3 + 1;
                                     progress = progress.Remove(indexReplace, 1).Insert(indexReplace, Convert.ToString(guess));
                                 }
                             }
@@ -190,8 +218,10 @@ namespace Hangman
                         }
                         if (!progress.Contains("_"))
                         {
+                            Console.BackgroundColor = ConsoleColor.Green;
                             Console.Clear();
                             Console.WriteLine("YOU WIN!!!");
+                            Music();
                             valid = false;
                             while (!valid)
                             {
@@ -242,11 +272,13 @@ namespace Hangman
                         if (word.Contains(guess))
                         {
                             indexReplace = 0;
+                            indexString = -1;
                             foreach (char c in word)
                             {
                                 if (c == guess)
                                 {
-                                    indexReplace = (word.IndexOf(c, indexReplace)) * 3 + 1;
+                                    indexString = word.IndexOf(c, indexString + 1);
+                                    indexReplace = indexString * 3 + 1;
                                     progress = progress.Remove(indexReplace, 1).Insert(indexReplace, Convert.ToString(guess));
                                 }
                             }
@@ -257,8 +289,10 @@ namespace Hangman
                         }
                         if (!progress.Contains("_"))
                         {
+                            Console.BackgroundColor = ConsoleColor.Green;
                             Console.Clear();
                             Console.WriteLine("YOU WIN!!!");
+                            Music();
                             valid = false;
                             while (!valid)
                             {
@@ -309,11 +343,13 @@ namespace Hangman
                         if (word.Contains(guess))
                         {
                             indexReplace = 0;
+                            indexString = -1;
                             foreach (char c in word)
                             {
                                 if (c == guess)
                                 {
-                                    indexReplace = (word.IndexOf(c, indexReplace)) * 3 + 1;
+                                    indexString = word.IndexOf(c, indexString + 1);
+                                    indexReplace = indexString * 3 + 1;
                                     progress = progress.Remove(indexReplace, 1).Insert(indexReplace, Convert.ToString(guess));
                                 }
                             }
@@ -324,8 +360,10 @@ namespace Hangman
                         }
                         if (!progress.Contains("_"))
                         {
+                            Console.BackgroundColor = ConsoleColor.Green;
                             Console.Clear();
                             Console.WriteLine("YOU WIN!!!");
+                            Music();
                             valid = false;
                             while (!valid)
                             {
@@ -376,11 +414,13 @@ namespace Hangman
                         if (word.Contains(guess))
                         {
                             indexReplace = 0;
+                            indexString = -1;
                             foreach (char c in word)
                             {
                                 if (c == guess)
                                 {
-                                    indexReplace = (word.IndexOf(c, indexReplace)) * 3 + 1;
+                                    indexString = word.IndexOf(c, indexString + 1);
+                                    indexReplace = indexString * 3 + 1;
                                     progress = progress.Remove(indexReplace, 1).Insert(indexReplace, Convert.ToString(guess));
                                 }
                             }
@@ -391,8 +431,10 @@ namespace Hangman
                         }
                         if (!progress.Contains("_"))
                         {
+                            Console.BackgroundColor = ConsoleColor.Green;
                             Console.Clear();
                             Console.WriteLine("YOU WIN!!!");
+                            Music();
                             valid = false;
                             while (!valid)
                             {
@@ -443,11 +485,13 @@ namespace Hangman
                         if (word.Contains(guess))
                         {
                             indexReplace = 0;
+                            indexString = -1;
                             foreach (char c in word)
                             {
                                 if (c == guess)
                                 {
-                                    indexReplace = (word.IndexOf(c, indexReplace)) * 3 + 1;
+                                    indexString = word.IndexOf(c, indexString + 1);
+                                    indexReplace = indexString * 3 + 1;
                                     progress = progress.Remove(indexReplace, 1).Insert(indexReplace, Convert.ToString(guess));
                                 }
                             }
@@ -458,8 +502,10 @@ namespace Hangman
                         }
                         if (!progress.Contains("_"))
                         {
+                            Console.BackgroundColor = ConsoleColor.Green;
                             Console.Clear();
                             Console.WriteLine("YOU WIN!!!");
+                            Music();
                             valid = false;
                             while (!valid)
                             {
@@ -483,6 +529,8 @@ namespace Hangman
                     }
                     else if (incorrect == 6)
                     {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Clear();
                         Console.WriteLine("  +---+");
                         Console.WriteLine("  |   |");
                         Console.WriteLine("  O   |");
@@ -514,6 +562,57 @@ namespace Hangman
                     }
                 }
             }
+        }
+        public static void Music()
+        {
+            Console.Beep(369, 200);
+            Console.Beep(369, 200);
+            Console.Beep(369, 200);
+            Console.Beep(293, 200);
+            Console.Beep(246, 200);
+            Console.Beep(329, 200);
+            Console.Beep(329, 200);
+            Console.Beep(329, 200);
+            Console.Beep(415, 200);
+            Console.Beep(415, 200);
+            Console.Beep(440, 200);
+            Console.Beep(493, 200);
+            Console.Beep(440, 200);
+            Console.Beep(440, 200);
+            Console.Beep(440, 200);
+            Console.Beep(329, 200);
+            Console.Beep(293, 200);
+            Console.Beep(369, 200);
+            Console.Beep(369, 200);
+            Console.Beep(369, 200);
+            Console.Beep(329, 200);
+            Console.Beep(329, 200);
+            Console.Beep(369, 200);
+            Console.Beep(329, 200);
+            Console.Beep(369, 200);
+            Console.Beep(369, 200);
+            Console.Beep(369, 200);
+            Console.Beep(293, 200);
+            Console.Beep(246, 200);
+            Console.Beep(329, 200);
+            Console.Beep(329, 200);
+            Console.Beep(329, 200);
+            Console.Beep(415, 200);
+            Console.Beep(415, 200);
+            Console.Beep(440, 200);
+            Console.Beep(493, 200);
+            Console.Beep(440, 200);
+            Console.Beep(440, 200);
+            Console.Beep(440, 200);
+            Console.Beep(329, 200);
+            Console.Beep(293, 200);
+            Console.Beep(369, 200);
+            Console.Beep(369, 200);
+            Console.Beep(369, 200);
+            Console.Beep(329, 200);
+            Console.Beep(329, 200);
+            Console.Beep(369, 200);
+            Console.Beep(329, 200);
         }
     }
 }
